c14cf376a3c15e6147be959cba419c0499f3ad77
dimaguy <afonsoantonio2003@gmail.com>
2023-09-08 09:22:01 +0100

    Upload files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # smtprestbridge
-SMTP-Rest Bridge designed to work with cloudflare-email API
+Simple SMTP-Rest Bridge designed to work with [cloudflare-email](https://github.com/dimaguy/cloudflare-email) API.  
+It tries to cap emails at 20MB, but it's untested.  
+Attachments are discarded for now (A workaround of automatically uploading them somewhere else and placing a link on the email body is in the roadmap)  
+TLS isn't supported yet.  
+Only HTTPS is supported for the REST API(fetch() is on the roadmap).  
+Multiple recipients is untested.
+BCC and CC is untested.
+Reply-To is untested.
+Sender and recipient names are untested.
+HTML emails aren't supported yet.
+
+## Environment variables used
+(You can use an .env file)
+- SMTPHOST - SMTP Host
+- SMTPPORT - SMTP Port (Recommended: 25)
+- EMAILDOMAIN - Sender email domain
+- SMTPAUTHOPTIONAL - SMTP Auth Optional
+- SMTPAUTHUSER - SMTP Auth User
+- SMTPAUTHPASS - SMTP Auth Password
+- RESTHOST - REST Host
+- RESTPORT - REST Port
+- RESTTOKEN - REST Token
---
+++ index.js
@@ -0,0 +1,73 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+const simpleParser = require('mailparser').simpleParser;
+const https = require("https");
+require('dotenv').config();
+const RESToptions = {
+    hostname: process.env.RESTHOST,
+    port: process.env.RESTPORT,
+    path: "/api/email",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": process.env.RESTTOKEN
+    }
+}
+const SMTPoptions = {
+    size: 1024 * 1024 * 20, //size limit of 20MB
+    secure: false,
+    authOptional: process.env.SMTPAUTHOPTIONAL,
+    allowInsecureAuth: true,
+    onAuth(auth, session, callback) {
+        if (auth.username !== process.env.SMTPAUTHUSER || auth.password !== process.env.SMTPAUTHPASS) {
+          return callback(new Error("Invalid username or password"));
+        }
+        callback(null, { user: 1 }); // where 1 is the user id or similar property
+      },
+    onMailFrom(address, session, callback) {
+        if (address.address.endsWith(process.env.EMAILDOMAIN)) {
+            return callback();
+        } else {
+            return callback(new Error("Invalid sender address"));
+        }
+    },
+    onData(stream, session, callback) {
+        let err;
+        stream.on("end", () => {
+            if (stream.sizeExceeded) {
+              err = new Error("Message exceeds fixed maximum message size");
+              err.responseCode = 552;
+              return callback(err);
+        }});
+        simpleParser(stream, {}, (err, parsed) => {
+            let mail = {
+                from: parsed.from.value[0],
+                to: (parsed.to != undefined) ? parsed.to.value : undefined,
+                subject: (parsed.subject != undefined) ? parsed.subject : "(No Subject)",
+                text: parsed.text,
+                bcc: (parsed.bcc != undefined) ? parsed.bcc.value : undefined,
+                cc: (parsed.cc != undefined) ? parsed.cc.value : undefined,
+                replyTo: (parsed.replyTo != undefined) ? parsed.replyTo.value : undefined
+            }
+            console.log(JSON.stringify(mail));
+            RESToptions.headers["Content-Length"] = JSON.stringify(mail).length;
+            console.log(RESToptions);
+            const req = https.request(RESToptions, (res) => {
+                console.log(res.statusCode);
+                req.on("data", d => {process.stdout.write(d)});
+                if (res.statusCode !== 200) {
+                    return callback(new Error("Bad response from REST API: " + res.statusCode));
+                }
+        })
+            req.on("error", err => {console.log("Error: " + err.message)});
+            req.write(JSON.stringify(mail));
+            req.end();
+            callback(null, "Message queued as ordered"); // accept the message once the stream is ended
+        });
+      },
+}
+const server = new SMTPServer(SMTPoptions);
+server.on("error", (err) => {
+    console.log("Error %s", err.message);
+  });
+server.listen(process.env.SMTPPORT, process.env.SMTPHOST);
+console.log("SMTP Server listening on " + process.env.SMTPHOST + ":" + process.env.SMTPPORT);
